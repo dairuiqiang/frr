7084f3b38648655385ac5b73d18d6c18b729d4d4
dairuiqiang <57743775+dairuiqiang@users.noreply.github.com>
2026-04-14 09:37:15 +0800

    Add BMP worker thread header definitions

---
+++ bgpd/bgp_bmp_worker.h
@@ -0,0 +1,10 @@
+#ifndef BGP_BMP_WORKER_H
+#define BGP_BMP_WORKER_H
+
+// BMP worker thread header definitions
+
+// Function declarations
+void start_bmp_worker_thread();
+void stop_bmp_worker_thread();
+
+#endif // BGP_BMP_WORKER_H
